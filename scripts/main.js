c52b93161f490ddd6214b7c2b440d9dbd38ab08d
zyb1412 <1412850457@qq.com>
2024-09-11 19:51:37 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "欢迎，" + myName+"来到FATE";
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "欢迎，" + storedName+"来到FATE";
+  }
+  
+  myButton.onclick = function () {
+    setUserName();
+  };
+  
